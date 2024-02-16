13252b1e08e097c8787fe2e327991dc38dc204ad
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-02-16 14:25:30 +0000

    styles: update tailwind config with app tokens

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-
-
 function App() {
   
 
   return (
-    <>
-      <main className=" text-red-400">Dev jobs app</main>
-    </>
+    <main className=" text-violet font-bold">
+      <h1>Lorem.</h1>
+      <h2>Lorem.</h2>
+      <h3 >Lorem.</h3>
+      <h4>Lorem.</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quidem amet ut, ducimus pariatur, ipsam nesciunt excepturi, numquam earum consequatur mollitia assumenda animi laboriosam asperiores explicabo praesentium illo totam voluptatibus! Necessitatibus placeat voluptates nam fuga sed, sit minus. Assumenda reprehenderit, nam aliquam ipsa sint voluptates!</p>
+    </main>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Kumbh Sans", "sans-serif"],
+      },
+    },
+    colors: {
+      violet: '#5964E0',
+      "light-violet": "#939BF4",
+      "dark-blue": '#19202D',
+      midnight: "#121721",
+      "light-grey": "#979797",
+      "gray": "#9DAEC2",
+      "dark-grey": "#6E8098"
+    },
+    fontSize: {
+      body: ["16px", { lineHeight: "26px", letterSpacing: "0" }],
+      h4: ["14px", { lineHeight: "18px", letterSpacing: "0" }],
+      h3: ["20px", { lineHeight: "24px", letterSpacing: "-0.8px" }],
+      h2: ["24px", { lineHeight: "29px", letterSpacing: "-0.63px" }],
+      h1: ["28px", { lineHeight: "34px", letterSpacing: "-0.1px" }],
+    },
   },
   plugins: [],
 }
