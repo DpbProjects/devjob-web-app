680e6cd40ce0b58ceaac6f57bbd303634d192a30
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-02-18 19:14:17 +0000

    feat: basic layout for the job search bar

---
+++ src/components/modules/JobSearchBar.tsx
@@ -0,0 +1,8 @@
+export default function JobSearchBar() {
+  return (
+    <div className=" top-[-46px] absolute w-full border-dashed border-2 border-gray min-h-20">
+      {/* Search bar */}
+      {/* Filter options */}
+    </div>
+  );
+}
