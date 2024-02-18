df017714ff5f7f5ed13223631176c97926e97f05
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-02-18 14:05:59 +0000

    feat: create layout component

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+
+import Header from "./Header";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+}
