88546c239cadea96832f18a9c9ecb12ea38a3f2b
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-02-18 06:09:31 +0000

    fix: index page import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // pages
-import IndexPage from "./pages";
+import IndexPage from "./pages/index";
 import JobsDetailsPage from "./pages/jobs-details";
 
 function App() {
