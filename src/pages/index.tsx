e8bd26e7b6446db47963f53394d937d2456e0e0f
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-02-18 19:14:47 +0000

    feat: add grid layout on the index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
+import JobList from "../components/modules/JobList";
+import JobSearchBar from "../components/modules/JobSearchBar";
+
 export default function IndexPage() {
-  return <main className=" bg-off-white dark:bg-midnight h-screen">index</main>;
+  return (
+    <div className="bg-off-white dark:bg-midnight relative">
+      <main className=" relative h-screen m-auto max-w-[327px] sm:max-w-[689px] lg:max-w-[1110px]">
+        <JobSearchBar />
+        <JobList />
+      </main>
+    </div>
+  );
 }
