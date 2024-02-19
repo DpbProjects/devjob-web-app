e56b45128f9c5dd6dbd9aa37f2b6752276a0f206
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-02-19 02:26:54 +0000

    feat: import job card to job list

--- src/components/modules/JobList.tsx
+++ src/components/modules/JobList.tsx
@@ -1,15 +1,21 @@
-export default function JobList() {
-  const grid = new Array(12).fill("grid");
+import data from "../../content/data.json";
 
+import JobCard from "./JobCard";
+
+export default function JobList() {
   return (
     <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-x-8 gap-y-16 min-h-full pt-36">
-      {grid.map((item, index) => (
-        <div
-          className="justify-center border-dashed border-2 border-gray"
-          key={index}
-        >
-          {item}
-        </div>
+      {data.map((item) => (
+        <JobCard
+          key={item.id}
+          position={item.position}
+          company={item.company}
+          postedAt={item.postedAt}
+          contract={item.contract}
+          location={item.location}
+          logo={item.logo}
+          logoBackground={item.logoBackground}
+        />
       ))}
     </div>
   );
