22acaeeff54bb796e2bd56b6b9bc4d46460bc5b2
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-02-19 02:27:29 +0000

    fix: make index page full height

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import JobSearchBar from "../components/modules/JobSearchBar";
 export default function IndexPage() {
   return (
     <div className="bg-off-white dark:bg-midnight relative">
-      <main className=" relative h-screen m-auto max-w-[327px] sm:max-w-[689px] lg:max-w-[1110px]">
+      <main className=" relative h-full m-auto max-w-[327px] sm:max-w-[689px] lg:max-w-[1110px]">
         <JobSearchBar />
         <JobList />
       </main>
